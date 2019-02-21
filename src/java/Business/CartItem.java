1ced1db3fa987c43b647915a405a5d3128706cb7
D00197085 <D00197085@P1111-PC31.ad.dkit.ie>
2019-02-21 11:35:29 +0000

    cart item initial work

--- src/java/Business/CartItem.java
+++ src/java/Business/CartItem.java
@@ -36,6 +36,11 @@ public class CartItem {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
+
+    @Override
+    public String toString() {
+        return "CartItem{" + "product=" + product + ", quantity=" + quantity + '}';
+    }
     
     
 }
